2ce51cee56073c3428f3768d8f07c5149c05891c
Marco Antônio de Freitas Gava <marcogava192425@gmail.com>
2025-07-29 16:03:11 -0300

    Pacote Config

--- api/main.go
+++ api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"api/src/config"
 	"api/src/router"
 	"fmt"
 	"log"
@@ -8,8 +9,12 @@ import (
 )
 
 func main() {
+	config.Carregar()
+	
+	fmt.Println(config.StringConexaoBanco)
+
 	fmt.Println("Rodando API!")
 	r := router.Gerar()
 
-	log.Fatal(http.ListenAndServe(":5000", r)) //Roda a API no endereço 5000
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r)) //Roda a API
 }
