bf467e85fa2326d4bf0702dfa8838309659fb8ab
Marco Antônio de Freitas Gava <marcogava192425@gmail.com>
2025-08-03 18:34:10 -0300

    Login

---
+++ api/src/controllers/login.go
@@ -0,0 +1,49 @@
+package controllers
+
+import (
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"api/src/respostas"
+	"api/src/seguranca"
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+// Login é responsável por autenticar o usuário na API
+func Login(w http.ResponseWriter, r *http.Request) {
+	corpoRequisicao, erro := io.ReadAll(r.Body) //lê o corpo da requisição
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro) //chama a função erro lá do respostas.go e envia o erro para o usuario
+		return
+	}
+
+	var usuario modelos.Usuario
+	if erro = json.Unmarshal(corpoRequisicao, &usuario); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro) //chama a função erro lá do respostas.go e envia o erro para o usuario
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro) //chama a função erro
+		return
+	}
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	usuarioSalvoNoBanco, erro := repositorio.BuscarPorEmail(usuario.Email)
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	if erro = seguranca.VerificarSenha(usuarioSalvoNoBanco.Senha, usuario.Senha); erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	w.Write([]byte("Você está logado! Parabéns!"))
+
+}
--- api/src/router/rotas/rotas.go
+++ api/src/router/rotas/rotas.go
@@ -17,6 +17,7 @@ type Rota struct {
 //Configurar coloca todas as rotas dentro do router
 func Configurar(r *mux.Router) *mux.Router {
 	rotas := rotasUsuarios //Pega as rotas definidas
+	rotas = append(rotas, rotaLogin) //Adiciona a rota de login
 
 	for _, rota := range rotas { //Para cada rota...
 		r.HandleFunc(rota.URI, rota.Funcao).Methods(rota.Metodo) //Registra no router: "quando vier requisição X, chame a função Y"
