ce28ef3bdba54fedfd551aa300b04cff3216b0f3
Marco Antônio de Freitas Gava <marcogava192425@gmail.com>
2025-08-06 12:36:23 -0300

    Tabela de seguidores

--- api/sql/sql.sql
+++ api/sql/sql.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE IF NOT EXISTS devbook;
 USE devbook;
 
+DROP TABLE IF EXISTS seguidores;
 DROP TABLE IF EXISTS usuarios;
 
 CREATE TABLE usuarios(
@@ -10,4 +11,18 @@ CREATE TABLE usuarios(
     email varchar(50) not null unique,
     senha varchar(100) not null,
     criadoEm timestamp default current_timestamp()
+) ENGINE=INNODB;
+
+CREATE TABLE seguidores(
+    usuario_id int not null,
+    FOREIGN KEY (usuario_id)
+    REFERENCES usuarios(id)
+    ON DELETE CASCADE,
+
+    seguidor_id int not null,
+    FOREIGN KEY (seguidor_id)
+    REFERENCES usuarios(id)
+    ON DELETE CASCADE,
+
+    primary key(usuario_id, seguidor_id)
 ) ENGINE=INNODB;
