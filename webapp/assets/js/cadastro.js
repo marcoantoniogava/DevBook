2777447647f8bf2901b3b9a7b344dd3cbe5719bd
Marco Antônio de Freitas Gava <marcogava192425@gmail.com>
2025-09-05 14:50:38 -0300

    Pacote Respostas

--- webapp/assets/js/cadastro.js
+++ webapp/assets/js/cadastro.js
@@ -17,6 +17,10 @@ function criarUsuario(evento) {
             nick: $("#nick").val(),
             senha: $("#senha").val(),
         }
+    }).done(function() { //201 200 204
+        console.log("Usuário cadastrado com sucesso!");
+    }).fail(function(erro) { //400 404 401 403 500
+        console.log(erro)
+        console.log("Erro ao cadastrar usuário!");
     });
-};
-
+}
