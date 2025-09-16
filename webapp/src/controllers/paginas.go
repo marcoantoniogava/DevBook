799325bdea713fc77b67fe48b9936c1cf788c35f
Marco Antônio de Freitas Gava <marcogava192425@gmail.com>
2025-09-16 14:58:05 -0300

    Requisição com autenticação para a API

--- webapp/src/controllers/paginas.go
+++ webapp/src/controllers/paginas.go
@@ -1,21 +1,28 @@
 package controllers
 
 import (
+	"fmt"
 	"net/http"
+	"webapp/src/config"
+	"webapp/src/requisicoes"
 	"webapp/src/utils"
 )
 
-//CarregarTelaDeLogin vai carregar a tela de login
+// CarregarTelaDeLogin vai carregar a tela de login
 func CarregarTelaDeLogin(w http.ResponseWriter, r *http.Request) {
 	utils.ExecutarTemplate(w, "login.html", nil)
 }
 
-//CarregarPaginaDeCadastroDeUsuario vai carregar a página de cadastro de usuário
-func CarregarPaginaDeCadastroDeUsuario(w http.ResponseWriter, r *http.Request)  {
+// CarregarPaginaDeCadastroDeUsuario vai carregar a página de cadastro de usuário
+func CarregarPaginaDeCadastroDeUsuario(w http.ResponseWriter, r *http.Request) {
 	utils.ExecutarTemplate(w, "cadastro.html", nil)
 }
 
-//CarregarPaginaPrincipal carrega a página principal com as publicações
-func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request)  {
+// CarregarPaginaPrincipal carrega a página principal com as publicações
+func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
+	url := fmt.Sprintf("%s/publicacoes", config.APIURL)
+	response, erro := requisicoes.FazerRequisicaoComAutenticacao(r, http.MethodGet, url, nil)
+	fmt.Println(response.StatusCode, erro)
+
 	utils.ExecutarTemplate(w, "home.html", nil)
 }
