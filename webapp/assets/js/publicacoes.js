29f15c4a3406e5d62d9a6119e23550baacc3fd6d
Marco Antônio de Freitas Gava <marcogava192425@gmail.com>
2025-09-26 16:07:33 -0300

    Curtir Publicação

--- webapp/assets/js/publicacoes.js
+++ webapp/assets/js/publicacoes.js
@@ -1,5 +1,6 @@
-$("#nova-publicacao").on("submit", criarPublicacao);
-
+$("#nova-publicacao").on("submit", criarPublicacao); //# pra id, . pra classe
+$(".curtir-publicacao").on("click", curtirPublicacao);
+console.log("publicacoes.js carregado");
 function criarPublicacao(evento) {
     evento.preventDefault();
 
@@ -15,4 +16,26 @@ function criarPublicacao(evento) {
     }).fail(function() {
         console.log("Erro ao criar publicação.");
     })
+}
+
+function curtirPublicacao(evento) {
+    evento.preventDefault();
+
+    const elementoClicado = $(evento.target);
+    const publicacaoId = elementoClicado.closest("div").data("publicacao-id");
+
+    elementoClicado.prop("disabled", true);
+    $.ajax({
+        url: `/publicacoes/${publicacaoId}/curtir`,
+        method: "POST"
+    }).done(function() {
+        const contadorDeCurtidas = elementoClicado.next("span");
+        const quantidadeDeCurtidas = parseInt(contadorDeCurtidas.text());
+
+        contadorDeCurtidas.text(quantidadeDeCurtidas + 1);
+    }).fail(function() {
+        console.log("Erro ao curtir a publicação!");
+    }).always(function() {
+        elementoClicado.prop("disabled", false);
+    });
 }
