d24f1512d474a8b4c493324c4026ead2c7a401f6
Marco Antônio de Freitas Gava <marcogava192425@gmail.com>
2025-09-28 15:10:57 -0300

    Descurtir Publicação

--- webapp/assets/js/publicacoes.js
+++ webapp/assets/js/publicacoes.js
@@ -1,6 +1,8 @@
 $("#nova-publicacao").on("submit", criarPublicacao); //# pra id, . pra classe
-$(".curtir-publicacao").on("click", curtirPublicacao);
-console.log("publicacoes.js carregado");
+
+$(document).on("click", ".curtir-publicacao", curtirPublicacao);
+$(document).on("click", ".descurtir-publicacao", descurtirPublicacao);
+
 function criarPublicacao(evento) {
     evento.preventDefault();
 
@@ -11,9 +13,9 @@ function criarPublicacao(evento) {
             titulo: $("#titulo").val(),
             conteudo: $("#conteudo").val()
         }
-    }).done(function() {
+    }).done(function () {
         window.location = "/home";
-    }).fail(function() {
+    }).fail(function () {
         console.log("Erro ao criar publicação.");
     })
 }
@@ -28,14 +30,46 @@ function curtirPublicacao(evento) {
     $.ajax({
         url: `/publicacoes/${publicacaoId}/curtir`,
         method: "POST"
-    }).done(function() {
+    }).done(function () {
         const contadorDeCurtidas = elementoClicado.next("span");
         const quantidadeDeCurtidas = parseInt(contadorDeCurtidas.text());
 
         contadorDeCurtidas.text(quantidadeDeCurtidas + 1);
-    }).fail(function() {
+
+        elementoClicado.addClass("descurtir-publicacao");
+        elementoClicado.addClass("text-danger");
+        elementoClicado.removeClass("curtir-publicacao");
+
+    }).fail(function () {
         console.log("Erro ao curtir a publicação!");
-    }).always(function() {
+    }).always(function () {
+        elementoClicado.prop("disabled", false);
+    });
+}
+
+function descurtirPublicacao(evento) {
+    evento.preventDefault();
+
+    const elementoClicado = $(evento.target);
+    const publicacaoId = elementoClicado.closest("div").data("publicacao-id");
+
+    elementoClicado.prop("disabled", true);
+    $.ajax({
+        url: `/publicacoes/${publicacaoId}/descurtir`,
+        method: "POST"
+    }).done(function () {
+        const contadorDeCurtidas = elementoClicado.next("span");
+        const quantidadeDeCurtidas = parseInt(contadorDeCurtidas.text());
+
+        contadorDeCurtidas.text(quantidadeDeCurtidas - 1);
+
+        elementoClicado.removeClass("descurtir-publicacao");
+        elementoClicado.removeClass("text-danger");
+        elementoClicado.addClass("curtir-publicacao");
+
+    }).fail(function () {
+        console.log("Erro ao curtir a publicação!");
+    }).always(function () {
         elementoClicado.prop("disabled", false);
     });
 }
