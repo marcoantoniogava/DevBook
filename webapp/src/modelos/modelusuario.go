975c879f16ef61fb10da89ab9a274193a460d911
Marco Antônio de Freitas Gava <marcogava192425@gmail.com>
2025-10-06 23:47:40 -0300

    Página do Perfil do Usuário

--- webapp/src/modelos/modelusuario.go
+++ webapp/src/modelos/modelusuario.go
@@ -1,15 +1,48 @@
 package modelos
 
-import "time"
+import (
+	"net/http"
+	"time"
+)
 
 // Usuario representa uma pessoa utilizando a rede social
 type Usuario struct {
-	ID       uint64 `json:"id"`
-	Nome     string `json:"nome"`
-	Nick     string `json:"nick"`
-	Email    string `json:"email"`
-	CriadoEm time.Time `json:"criadoEm"`
-	Seguidores []Usuario `json:"seguidores"`
-	Seguindo   []Usuario `json:"seguindo"`
+	ID          uint64       `json:"id"`
+	Nome        string       `json:"nome"`
+	Nick        string       `json:"nick"`
+	Email       string       `json:"email"`
+	CriadoEm    time.Time    `json:"criadoEm"`
+	Seguidores  []Usuario    `json:"seguidores"`
+	Seguindo    []Usuario    `json:"seguindo"`
 	Publicacoes []Publicacao `json:"publicacoes"`
+}
+
+// BuscarUsuarioCompleto faz 4 requisições na API para montar o usuário
+func BuscarUsuarioCompleto(usuarioID uint64, r *http.Request) (Usuario, error) {
+	canalUsuario := make(chan Usuario)
+	canalSeguidores := make(chan []Usuario)
+	canalSeguindo := make(chan []Usuario)
+	canalPublicacoes := make(chan []Publicacao)
+
+	go BuscarDadosDoUsuario(canalUsuario, usuarioID, r)
+	go BuscarSeguidores(canalSeguidores, usuarioID, r)
+	go BuscarSeguindo(canalSeguindo, usuarioID, r)
+	go BuscarPublicacoes(canalPublicacoes, usuarioID, r)
+
+}
+
+func BuscarDadosDoUsuario(canal <-chan Usuario, usuarioID uint64, r *http.Request) {
+
+}
+
+func BuscarSeguidores(canal <-chan []Usuario, usuarioID uint64, r *http.Request) {
+
+}
+
+func BuscarSeguindo(canal <-chan []Usuario, usuarioID uint64, r *http.Request) {
+
+}
+
+func BuscarPublicacoes(canal <-chan []Publicacao, usuarioID uint64, r *http.Request) {
+
 }
